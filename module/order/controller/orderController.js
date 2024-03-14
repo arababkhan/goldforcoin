4252a4891b464fd12da3b0c24d47ba189afb5ea3
arababkhan <arababkhan998@gmail.com>
2024-03-14 16:41:47 +0700

    updated transaction check

--- module/order/controller/orderController.js
+++ module/order/controller/orderController.js
@@ -28,7 +28,7 @@ exports.addOrder = async function(req,res) {
     }  
     
     try {
-        const w_paidAmount = await getDepositDetail(req.body.order.chain, req.body.order.transaction)
+        const w_paidAmount = await getDepositDetail(req.body.order.chain, req.body.order.coin, req.body.order.transaction)
         let w_orderTemp = req.body.order;
         if(w_paidAmount > -1) {
             w_orderTemp = {...w_orderTemp, paid: w_paidAmount}
@@ -124,60 +124,100 @@ isEmptyObject = function (obj) {
     return true;
 }
 
-async function getDepositDetail(chain, txHash) {
+async function getDepositDetail(chain, coin, txHash) {
     try {
         let w_chain_rpc = ''
-        if(chain == 1) {
-            w_chain_rpc = process.env.eth_mainnet
-        } else if(chain == 5) {
-            w_chain_rpc = process.env.eth_testnet
-        } else if(chain == 56) {
-            w_chain_rpc = process.env.bsc_mainnet
-        } else if(chain == 97) {
-            w_chain_rpc = process.env.bsc_testnet
-        }
-        const web3 = new Web3(w_chain_rpc);
+        let w_contract = ''
+        let w_decimals = 1;
 
-        // Fetch the transaction receipt to get logs
-        const receipt = await web3.eth.getTransactionReceipt(txHash);
-        const depositEventAbi = [
+        const Contract_ABI = [
             {
-                "indexed": true,
-                "internalType": "address",
-                "name": "user",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "amount",
-                "type": "uint256"
-            },
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "currency",
-                "type": "string"
+              "constant": false,
+              "inputs": [
+                {
+                  "name": "_to",
+                  "type": "address"
+                },
+                {
+                  "name": "_value",
+                  "type": "uint256"
+                }
+              ],
+              "name": "transfer",
+              "outputs": [
+                {
+                  "name": "",
+                  "type": "bool"
+                }
+              ],
+              "type": "function"
             }
         ];
 
-        for (let log of receipt.logs) {
-            try {
-                const decoded = web3.eth.abi.decodeLog(depositEventAbi, log.data, log.topics.slice(1));
-                if (decoded) {
-                    let w_bigIntValue = BigInt(decoded.amount)
-                    let w_decimal = 18;
-                    if (chain == 1)
-                        w_decimal = 6;
-                    let w_paidAmount = coins(w_bigIntValue.toString(), w_decimal)
-                    return w_paidAmount;
-                    // console.log(`Deposit Details: Sender: ${log.topics[1]}, Recipient (Contract Address): ${receipt.to}, Amount: ${decoded.amount}, Currency (isUSDT): ${decoded.currency}`);
-                }
-            } catch (error) {
-
+        if(chain == 1) {
+            w_chain_rpc = process.env.eth_mainnet
+            w_decimals = 6;
+            if(coin == 'usdt') {
+                w_contract = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+            } else if(coin == 'usdc') {
+                w_contract = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+            }
+        } else if(chain == 5) {
+            w_decimals = 6;
+            w_chain_rpc = process.env.eth_testnet
+            if(coin == 'usdt') {
+                w_contract = '0x94829DD28aE65bF4Ff6Ce3A687B1053eC7229272';
+            } else if(coin == 'usdc') {
+                w_contract = '0x07865c6e87b9f70255377e024ace6630c1eaa37f';
+            }
+        } else if(chain == 56) {
+            w_decimals = 18;
+            w_chain_rpc = process.env.bsc_mainnet
+            if(coin == 'usdt') {
+                w_contract = '0x55d398326f99059fF775485246999027B3197955';
+            } else if(coin == 'usdc') {
+                w_contract = '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d';
+            }
+        } else if(chain == 97) {
+            w_decimals = 18;
+            w_chain_rpc = process.env.bsc_testnet
+            if(coin == 'usdt') {
+                w_contract = '0xCA7ff3eBFceC8869814614351a58bF8f18CE8bc7';
+            } else if(coin == 'usdc') {
+                w_contract = '0x64544969ed7ebf5f083679233325356ebe738930';
             }
         }
-        return -1;
+        const web3 = new Web3(w_chain_rpc);
+        // Fetch the transaction receipt to get logs
+        const tx = await web3.eth.getTransaction(txHash);
+        if (!tx) {
+            console.log('Transaction not found');
+            return -1;
+        }
+        if (tx.to && tx.to.toLowerCase() === w_contract.toLowerCase()) {
+            const contract = new web3.eth.Contract(Contract_ABI, w_contract);
+            const inputData = tx.input;
+            
+            const methodSignature = inputData.slice(0,10); // First 4 bytes is the method ID
+            const [method] = contract.options.jsonInterface.filter((method) => method.signature === methodSignature);
+        
+            if (method && method.name === "transfer") {
+              const decodedParams = web3.eth.abi.decodeParameters(method.inputs, '0x' + inputData.slice(10));
+              if(w_decimals == 6){
+                console.log(`USDT Transferred: ${web3.utils.fromWei(decodedParams._value, 'mwei')} to address: ${decodedParams._to}`);
+                return web3.utils.fromWei(decodedParams._value, 'mwei')
+              } else if(w_decimals == 18) {
+                console.log(`USDT Transferred: ${web3.utils.fromWei(decodedParams._value, 'ether')} to address: ${decodedParams._to}`);
+                return web3.utils.fromWei(decodedParams._value, 'ether')
+              }
+            } else {
+              console.log('Transaction is not a USDT transfer.');
+              return -1;
+            }
+        } else {
+            console.log('Transaction is not related to the USDT contract.');
+            return -1;
+        }
     } catch (error) {
         console.error("Error fetching transaction receipt:", error);
         return -1;
